b269305fc45246221bea8cafab33669846adb54b
bhgupta78 <guptabharat790@gmail.com>
2022-04-21 21:40:22 +0530

    intial commit

---
+++ dulpicate.js
@@ -0,0 +1,9 @@
+const a1 =[1,3,4,5,56];
+const copyofArray1 =a1.slice();
+const  array = [5,6,7,8];
+
+console.log(a1);
+console.log(copyofArray1);
+
+const a3= a1.concat(copyofArray1);
+console.log( a3 );
