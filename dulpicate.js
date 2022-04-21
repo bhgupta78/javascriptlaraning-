e2745dcc3cc756ae4f30718128e95b8a34882fa7
bhgupta78 <guptabharat790@gmail.com>
2022-04-21 22:13:41 +0530

    sahi kiya

--- dulpicate.js
+++ dulpicate.js
@@ -1,9 +1,11 @@
 const a1 =[1,3,4,5,56];
 const copyofArray1 =a1.slice();
 const  array = [5,6,7,8,9,99];
+const q99 = [2,8,9,10,88];
 
 console.log(a1);
 console.log(copyofArray1);
 
 const a3= a1.concat(copyofArray1);
-console.log( a3 );
+const a4=a3.concat(q99)
+console.log( a4 );
